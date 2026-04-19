a33d5be9415470c3a651c9372338bdd11330e319
ryanburkii <burkiiryan@gmail.com>
2026-04-18 21:20:42 -0400

    fix(terminus): drop redundant verified: parameter

--- Casks/terminus.rb
+++ Casks/terminus.rb
@@ -2,8 +2,7 @@ cask "terminus" do
   version "0.1.0"
   sha256 "4056b815d017af7a8c372842ea8aab82c63984955274f9a4e6e262a944266dea"
 
-  url "https://github.com/juliushamm/terminus/releases/download/v#{version}/terminus-#{version}.dmg",
-      verified: "github.com/juliushamm/terminus/"
+  url "https://github.com/juliushamm/terminus/releases/download/v#{version}/terminus-#{version}.dmg"
   name "Terminus"
   desc "Visual AWS infrastructure management"
   homepage "https://github.com/juliushamm/terminus"
